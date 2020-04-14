995744a7eaec9c2a2339e5bdff4d4ba3ca2700fb
祝泽宇 <734047890@qq.com>
2020-04-14 22:22:35 +0800

    c代码简单练习

---
+++ test 3 11/test 3 11/test.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	printf("hello bit\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//	printf("%d\n", age);
+//	return 0;
+//}
+
+//int main()
+//{
+//	float weight = 49.6f;
+//	printf("weight = %f\n", weight);
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d\n", sizeof(char));
+//	printf("%d\n", sizeof(short));
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof(long));
+//	printf("%d\n", sizeof(long long));
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(double));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int max = 10000;
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//	printf("请输入两个加数:>");
+//	scanf("%d%d", &num1, &num2);
+//	sum = num1+num2;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
+//extern int g_val;
+//int main()
+//{
+//	printf("g_val = %d\n", g_val);
+//	return 0;
+//}
+
+int main()
+{
+	{
+		int a = 10;
+		printf("a = %d\n", a);
+	}
+	//不能使用
+	printf("a = %d\n", a);
+	return 0;
+}
